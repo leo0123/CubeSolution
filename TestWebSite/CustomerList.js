b7bceec5f60ca24f3b86b3791fb1387b35f46ac7
Leo <leo.c.chen@deltaww.com>
2016-12-22 16:02:23 -0800

    b4 add op

---
+++ TestWebSite/CustomerList.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    $scope.records = [
+       {
+           "Name": "Alfreds Futterkiste",
+           "Country": "Germany"
+       }, {
+           "Name": "Berglunds snabbköp",
+           "Country": "Sweden"
+       }, {
+           "Name": "Centro comercial Moctezuma",
+           "Country": "Mexico"
+       }, {
+           "Name": "Ernst Handel",
+           "Country": "Austria"
+       }
+    ]
+});
